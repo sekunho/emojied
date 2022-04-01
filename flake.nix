24bdee662e95033a52bc9d6f197b34a074c55e1e
SEKUN <sekun@hey.com>
2022-04-01 22:54:29 +0800

    wip

--- flake.nix
+++ flake.nix
@@ -5,15 +5,20 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
     # pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, nixos-unstable, flake-utils }:
+  outputs = { self, nixpkgs, nixos-unstable, flake-utils, naersk }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let pkgs = nixpkgs.legacyPackages.${system};
           unstablepkgs = nixos-unstable.legacyPackages.${system};
+          naersk-lib = naersk.lib."${system}".override {
+            cargo = pkgs.cargo;
+            rustc = pkgs.rustc;
+          };
       in
-      {
+      rec {
         # checks = {
         #   pre-commit-check = pre-commit-hooks.lib.${system}.run {
         #     src = ./.;
@@ -25,10 +30,23 @@
         #   };
         # };
 
+        packages.emojied = naersk-lib.buildPackage {
+          pname = "emojied";
+          root = ./.;
+        };
+
+        defaultPackage = packages.emojied;
+
+        apps.emojied = flake-utils.lib.mkApp {
+          drv = packages.emojied;
+        };
+
+        defaultApp = apps.emojied;
+
         devShell = pkgs.mkShell {
           # inherit (self.checks.${system}.pre-commit-check) shellHook;
 
-          buildInputs = with pkgs; [
+          nativeBuildInputs = with pkgs; [
             # Back-end
             pkgs.rustc
             pkgs.cargo
