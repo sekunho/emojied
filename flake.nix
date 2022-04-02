f26d6d71fa44c27cb0851fa9303d2202b3477856
SEKUN <sekun@hey.com>
2022-04-02 12:52:59 +0800

    wip: `crate2nix` route
    I think I prefer `naersk` cause the build times are much shorter
    but it still has its problems with symlinks.

--- flake.nix
+++ flake.nix
@@ -5,20 +5,31 @@
     nixpkgs.url = "github:NixOS/nixpkgs";
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nix-community/naersk?rev=bd4822e754eba04977667ab8a9e5314ae126cc1c";
+    crate2nix = {
+      url = "github:sekunho/crate2nix/sekunho/dedup-sources";
+      flake = false;
+    };
     # pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
-  outputs = { self, nixpkgs, nixos-unstable, flake-utils, naersk }:
+  outputs = { self, nixpkgs, nixos-unstable, flake-utils, crate2nix }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let pkgs = nixpkgs.legacyPackages.${system};
           unstablepkgs = nixos-unstable.legacyPackages.${system};
-          naersk-lib = naersk.lib."${system}".override {
-            cargo = pkgs.cargo;
-            rustc = pkgs.rustc;
+
+          crateName = "emojied";
+
+          inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
+            generatedCargoNix;
+
+          project = import (generatedCargoNix {
+            name = crateName;
+            src = ./.;
+          }) {
+            inherit pkgs;
+            defaultCrateOverrides = pkgs.defaultCrateOverrides // {};
           };
-      in
-      rec {
+      in {
         # checks = {
         #   pre-commit-check = pre-commit-hooks.lib.${system}.run {
         #     src = ./.;
@@ -30,20 +41,9 @@
         #   };
         # };
 
-        # NOTE: Build still fails the same way
-        packages.emojied = naersk-lib.buildPackage {
-          pname = "emojied";
-          root = ./.;
-          gitSubmodules = true;
-        };
+        packages.${crateName} = project.rootCrate.build;
 
-        defaultPackage = packages.emojied;
-
-        apps.emojied = flake-utils.lib.mkApp {
-          drv = packages.emojied;
-        };
-
-        defaultApp = apps.emojied;
+        defaultPackage = self.packages.${system}.${crateName};
 
         devShell = pkgs.mkShell {
           # inherit (self.checks.${system}.pre-commit-check) shellHook;
