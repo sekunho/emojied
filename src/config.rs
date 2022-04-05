9c85879f9b32f6e8c173cbdef993fd0e511085ad
SEKUN <sekun@hey.com>
2022-04-05 17:37:30 +0800

    feat: Add database pooling
    Pool size is configurable but defaults to 22, which is the recommended
    size by DO.

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,35 @@
 use std::env;
+use deadpool_postgres::{ManagerConfig, RecyclingMethod};
 
-pub struct AppConfig {
-    pub database_url: String,
+pub struct Config {
     pub host: String,
+    pub pg: tokio_postgres::Config,
+    pub manager: ManagerConfig,
+    pub pool_size: usize,
 }
 
-impl AppConfig {
-    pub fn new() -> Result<AppConfig, env::VarError>{
-        let database_url = env::var("DATABASE_URL")?;
-        let host = env::var("APP_HOST")?;
+#[derive(Debug)]
+pub enum Error {
+    VarError(env::VarError),
+}
 
-        Ok(AppConfig { database_url, host })
+impl Config {
+    pub fn from_env() -> Result<Config, Error>{
+        let mut pg_config = tokio_postgres::Config::new();
+        let manager_config = ManagerConfig {
+            recycling_method: RecyclingMethod::Fast
+        };
+
+        // TODO: Get from environment instead
+        pg_config.host("localhost");
+        pg_config.user("postgres");
+        pg_config.dbname("emojied_db");
+
+        Ok(Config {
+            host: "emojied.net".to_string(),
+            pg: pg_config,
+            manager: manager_config,
+            pool_size: 22,
+        })
     }
 }
